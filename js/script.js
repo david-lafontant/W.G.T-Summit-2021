aa3ad89dcdbbee0714396173535c36affad2f8fa
david-lafontant <lafontant.david@gmail.com>
2022-01-05 20:55:59 -0500

    Fix error in script

--- js/script.js
+++ js/script.js
@@ -4,13 +4,6 @@ const close = document.querySelector('.close');
 const open = document.querySelector('.open');
 const menuLinks = document.querySelectorAll('.menu-link');
 
-open.addEventListener('click', showNavbar);
-
-close.addEventListener('click', hideNavbar);
-menuLinks.forEach((item) => {
-  item.addEventListener('click', hideNavbar);
-});
-
 function showNavbar() {
   menu.style.display = 'flex';
   menu.style.top = '0';
@@ -19,3 +12,12 @@ function showNavbar() {
 function hideNavbar() {
   menu.style.top = '-100%';
 }
+
+
+open.addEventListener('click', showNavbar);
+
+close.addEventListener('click', hideNavbar);
+menuLinks.forEach((item) => {
+  item.addEventListener('click', hideNavbar);
+});
+

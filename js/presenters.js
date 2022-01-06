dbce5043877b813f5399a1697857fd1d28b0b9ce
david-lafontant <lafontant.david@gmail.com>
2022-01-06 17:39:11 -0500

    Fix logical errors in presenters.js

--- js/presenters.js
+++ js/presenters.js
@@ -1,6 +1,7 @@
 const zero = 0;
 const one = 1;
 const two = 2;
+const store = [];
 
 const speakersData = [
   {
@@ -72,44 +73,48 @@ const parentNode = document.querySelector('body');
 const existingNode1 = document.querySelector('.partners');
 const existingNode = document.querySelector('.more');
 
-speakersData.forEach((elem, num) => {
-  const speakerItem = document.createElement('div');
-  speakerItem.setAttribute('class', 'item');
+if ($(window).width() > 768) {
+  speakersData.forEach((elem, num) => {
+    store.push(elem);
 
-  const poster = document.createElement('img');
-  poster.setAttribute('class', 'posterImage');
-  poster.setAttribute('src', speakersData[num].source);
-  poster.setAttribute('alt', speakersData[num].altText);
-  speakerItem.appendChild(poster);
+    const speakerItem = document.createElement('div');
+    speakerItem.setAttribute('class', 'item');
 
-  const details = document.createElement('div');
-  details.className = 'detail';
+    const poster = document.createElement('img');
+    poster.setAttribute('class', 'posterImage');
+    poster.setAttribute('src', speakersData[num].source);
+    poster.setAttribute('alt', speakersData[num].altText);
+    speakerItem.appendChild(poster);
 
-  const theName = document.createElement('p');
-  theName.setAttribute('class', 'speakerName');
-  theName.textContent = speakersData[num].name;
-  details.appendChild(theName);
+    const details = document.createElement('div');
+    details.className = 'detail';
 
-  const speakerTitle = document.createElement('p');
-  speakerTitle.setAttribute('class', 'titleOfSpeaker');
-  speakerTitle.textContent = speakersData[num].title;
-  details.appendChild(speakerTitle);
+    const theName = document.createElement('p');
+    theName.setAttribute('class', 'speakerName');
+    theName.textContent = speakersData[num].name;
+    details.appendChild(theName);
 
-  const line = document.createElement('hr');
-  line.setAttribute('class', 'hrLine');
-  details.appendChild(line);
+    const speakerTitle = document.createElement('p');
+    speakerTitle.setAttribute('class', 'titleOfSpeaker');
+    speakerTitle.textContent = speakersData[num].title;
+    details.appendChild(speakerTitle);
 
-  const info = document.createElement('p');
-  info.setAttribute('class', 'descriptions');
-  info.textContent = speakersData[num].description;
-  details.appendChild(info);
+    const line = document.createElement('hr');
+    line.setAttribute('class', 'hrLine');
+    details.appendChild(line);
 
-  speakerItem.appendChild(details);
+    const info = document.createElement('p');
+    info.setAttribute('class', 'descriptions');
+    info.textContent = speakersData[num].description;
+    details.appendChild(info);
 
-  speakerWrapper.appendChild(speakerItem);
+    speakerItem.appendChild(details);
 
-  existingNode1.parentNode.insertBefore(speakers, existingNode1);
-});
+    speakerWrapper.appendChild(speakerItem);
+
+    existingNode1.parentNode.insertBefore(speakers, existingNode1);
+  });
+}
 
 if ($(window).width() <= 768) {
   for (let num = zero; num < two; num += one) {
